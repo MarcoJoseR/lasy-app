815d28c980cd1e229d08002de33407d47ddf14f7
MarcoJose <marcozej@gmail.com>
2026-08-23 20:27:19 -0300

    Adiciona enctype ao share target

--- app/manifest.ts
+++ app/manifest.ts
@@ -25,6 +25,7 @@ export default function manifest() {
     share_target: {
       action: "/importar-receita",
       method: "GET",
+      enctype: "application/x-www-form-urlencoded",
       params: {
         title: "title",
         text: "text",
